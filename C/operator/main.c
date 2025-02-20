10bf2212460252c5cbf3a3a09785a2c2bbbd532e
magic452 <kik4817@naver.com>
2025-02-20 17:48:57 +0900

    2025-02-20 Data

--- C/operator/main.c
+++ C/operator/main.c
@@ -107,11 +107,26 @@ int main()
 	//무기를 강화하는데
 	//n 강일때의 무기, n+1 무기의 정보를 화면에 출력하는 스토리를 작성
 	
-	int num5 = 0;
-	int num6 = 0;
+	
 	//printf("무기 %d \n", num5);
 	//printf("무기 %d \n", ++num5);
-	printf("무기 %d 에서 성공하여 %d \n", num5, ++num6);
+	
+	int maxInventorySlot = 10;
+	int AStackCount = 200;
+	int BStackCount = 200;
+	int ADrop = 555;
+	int BDrop = 7777;
+
+	printf("1시간 동안 사냥을 해서 아이템을 획득을 했다.\n");
+	printf("A아이템 : %d, B아이템 : %d 획득했다.\n", ADrop, BDrop);
+
+	// 나머지
+
+	int AInventoryCount = (ADrop % AStackCount) == 0 ? (ADrop / AStackCount) : (ADrop / AStackCount) + 1;
+	int BInventoryCount = (BDrop % BStackCount) == 0 ? (BDrop / BStackCount) : (BDrop / BStackCount) + 1;
+
+	printf(" A의 갯수 :%d B의 갯수 : %d\n", AInventoryCount, BInventoryCount);
+	printf("결과 :  %s\n", maxInventorySlot <= (AInventoryCount + BInventoryCount) ? "가득참" : "가득안참");
 
 	//게임 스토리 2
 	//아이템 인벤토리 slot 기타 아이템 수집, stack 갯수
@@ -120,10 +135,6 @@ int main()
 	//아이템 n개 획득, B 아이템 M개 획득
 	//최대 인벤토리의 갯수가 가득 찼는지 안찼는지 표한하는 결과를 표현
 
-	int slot1 = 5;
-	int stack1 = 1;
-	int bag = slot1 > stack1;
-	printf("가방");
 
-	//게임 스토리 3 작성
+		//게임 스토리 3 작성
 }
